1227024ae5e83c4a1f6811f71884e50f7661a7ae
Robin Sallis <Toutouwai@users.noreply.github.com>
2019-03-03 14:14:04 +1300

    v0.1.6
    Allows limit of zero items.

--- LimitRepeater.js
+++ LimitRepeater.js
@@ -2,7 +2,7 @@ function checkRepeater(role_class, inputfield_class, limit) {
 	if(!inputfield_class) return;
 	var $repeater_items = $('body' + role_class + ' ' + inputfield_class + ' .InputfieldRepeaterItem').not('.InputfieldRepeaterNewItem');
 	var $add_button = $('body' + role_class + ' ' + inputfield_class + ' > div > .InputfieldRepeaterAddItem');
-	if($repeater_items.length > limit - 1) {
+	if($repeater_items.length >= limit) {
 		$add_button.addClass('button-hide');
 	} else {
 		$add_button.removeClass('button-hide');
